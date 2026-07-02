ff908407dc83370226a93da4d40afdf3836e370c
ViniciusEduardoDias <diasvinicius0401@gmail.com>
2026-07-02 11:33:33 -0300

    feat:Montagem da estrutura do projeto

---
+++ plugin/bomsistema-catalogo/bomsistema-catalogo.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Bomsistema Catálogo
+ * Plugin URI: https://bomsistema.com.br
+ * Description: Catálogo de produtos em React.
+ * Version: 1.0.0
+ * Author: Bomsistema
+ * Author URI: https://bomsistema.com.br
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('BOMSISTEMA_CATALOGO_PATH', plugin_dir_path(__FILE__));
+define('BOMSISTEMA_CATALOGO_URL', plugin_dir_url(__FILE__));
+
+require_once BOMSISTEMA_CATALOGO_PATH . 'includes/init.php';
+
+function bomsistema_catalogo_shortcode()
+{
+    return '<div id="bomsistema-catalogo"></div>';
+}
+
+add_shortcode('bomsistema_catalogo', 'bomsistema_catalogo_shortcode');
---
+++ react/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Olá, Bomsistema 👋</h1>
+      <p>React funcionando.</p>
+    </div>
+  );
+}
+
+export default App;
